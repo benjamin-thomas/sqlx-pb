1fa899e4012e2f6cfa92a25b38e7fa17873b6eab
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-18 20:35:46 +0200

    Almost get next jobs (code compiles here)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use sqlx::{postgres::PgArguments, query::Query, PgPool, Pool, Postgres};
+use sqlx::postgres::PgRow;
+use sqlx::{
+    postgres::PgArguments,
+    query::{Map, Query},
+    Error, PgPool, Pool, Postgres,
+};
+
+struct Job {
+    id: i64,
+    // status: String,
+    // message: String,
+}
 
 async fn must_get_pool() -> Pool<Postgres> {
     PgPool::connect("postgres://postgres:leak-ok-ieQu5ahh4P@localhost:5433/my_app")
@@ -20,6 +31,25 @@ fn insert_jobs_query() -> Query<'static, Postgres, PgArguments> {
     )
 }
 
+fn next_jobs_query() -> Map<'static, Postgres, fn(PgRow) -> Result<Job, Error>, PgArguments> {
+    sqlx::query_as!(
+        Job,
+        r#"
+            UPDATE jobs
+            SET status = 'Running'
+            WHERE id IN (
+                SELECT id
+                FROM jobs
+                WHERE status = 'Queued'
+                ORDER BY id
+                LIMIT 5
+                FOR UPDATE SKIP LOCKED
+            )
+            RETURNING id
+            "#
+    )
+}
+
 #[tokio::main]
 async fn main() {
     let pg_pool = must_get_pool().await;
@@ -29,4 +59,10 @@ async fn main() {
         .await
         .expect("Could not insert jobs");
 
+    let _jobs: Vec<Job> = next_jobs_query()
+        .fetch_all(&pg_pool)
+        .await
+        .expect("Could not get jobs batch");
+
+    ()
 }
