b669a1a509ba4d9c4c02f0bf4e211f53826555a4
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-18 06:41:43 +0200

    Add migration

---
+++ migrations/20220518042858_create_jobs.sql
@@ -0,0 +1,9 @@
+CREATE TYPE JOB_STATUS AS ENUM ('Queued', 'Running', 'Failed');
+
+CREATE TABLE jobs
+(
+    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    status JOB_STATUS NOT NULL DEFAULT 'Queued',
+    message JSONB NOT NULL
+);
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Next step: '{}'", "INSERT INTO jobs (message) VALUES ('{}');");
 }
