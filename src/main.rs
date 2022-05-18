8bed572db942817120e53dac65aa0465e95eeb66
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-18 07:43:08 +0200

    Insert jobs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Next step: '{}'", "INSERT INTO jobs (message) VALUES ('{}');");
+use sqlx::PgPool;
+
+#[tokio::main]
+async fn main() {
+    let pg_pool = PgPool::connect("postgres://postgres:leak-ok-ieQu5ahh4P@localhost:5433/my_app")
+        .await
+        .expect("Could not connect to the database!");
+
+    println!("Inserting jobs...");
+    sqlx::query!(
+        r#"
+        INSERT INTO jobs (message)
+        VALUES ('{"a": 1}')
+             , ('{"b": 2}')
+             , ('{"c": 3}')
+             , ('{"d": 4}')
+             , ('{"e": 5}')
+    "#
+    )
+    .execute(&pg_pool)
+    .await
+    .expect("Could not insert job");
+
+    println!(
+        "Next step: '{}'",
+        "INSERT INTO jobs (message) VALUES ('{}');"
+    );
 }
