8b43638f8967e7460382865162bb75f0162bb643
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-18 22:26:01 +0200

    Stuck on error

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ pub enum Payload {
     SendEmail { email: String },
 }
 
+struct JobLight {
+    id: i64,
+}
+
 struct Job {
     id: i64,
     // status: String,
@@ -58,7 +62,7 @@ fn insert_jobs_query() -> Query<'static, Postgres, PgArguments> {
 //                 LIMIT 5
 //                 FOR UPDATE SKIP LOCKED
 //             )
-//             RETURNING id
+//             RETURNING id, payload
 //             "#
 //     )
 // }
@@ -76,7 +80,46 @@ async fn main() {
     //     .fetch_all(&pg_pool)
     //     .await
     //     .expect("Could not get jobs batch");
-    //
+    let jobs = sqlx::query_as!(
+        Job,
+        r#"
+            UPDATE jobs
+            SET status = 'Running'
+            WHERE id IN (
+                SELECT id
+                FROM jobs
+                WHERE status = 'Queued'
+                ORDER BY id
+                LIMIT 5
+                FOR UPDATE SKIP LOCKED
+            )
+            RETURNING id, payload
+            "#
+    );
+    /*
+    ERROR HERE:
+            Checking sqlx-pb v0.1.0 (/home/benjamin/code/explore/rust/sqlx-pb)
+    error[E0308]: mismatched types
+      --> src/main.rs:83:16
+       |
+    83 |       let jobs = sqlx::query_as!(
+       |  ________________^
+    84 | |         Job,
+    85 | |         r#"
+    86 | |             UPDATE jobs
+    ...  |
+    97 | |             "#
+    98 | |     );
+       | |_____^ expected enum `Payload`, found enum `serde_json::Value`
+       |
+       = note: this error originates in the macro `$crate::sqlx_macros::expand_query` (in Nightly builds, run with -Z macro-backtrace for more info)
+
+    For more information about this error, try `rustc --explain E0308`.
+    error: could not compile `sqlx-pb` due to previous error
+    [Finished running. Exit status: 101]
+
+         */
+
     // for j in jobs {
     //     println!("Will work on job #{}", j.id)
     // }
